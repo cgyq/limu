dd204e1d70cd76a0fe0481c5da28775f64116e6d
cgyq <1264642896@qq.com>
2021-12-05 15:59:38 +0800

    linear

--- PyTorch/linear_regression.py
+++ PyTorch/linear_regression.py
@@ -26,6 +26,43 @@ def data_iter(batch_size, features, labels):
 
 batch_size = 10
 
-for X, y in data_iter(batch_size, features, labels):
-    print(X, '\n', y)
-    break
+# for X, y in data_iter(batch_size, features, labels):
+#     print(X, '\n', y)
+#     break
+
+w = torch.normal(0, 0.01, size=(2, 1), requires_grad=True)
+b = torch.zeros(1, requires_grad=True)
+
+def linreg(X, w, b):
+    """线性回归模型"""
+    return torch.matmul(X, w) + b
+
+def squared_loss(y_hat, y):
+    """均方误差"""
+    return (y_hat - y.reshape(y_hat.shape))**2 / 2
+
+def sgd(params, lr, batch_size):
+    """小批量随机梯度下降"""
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad / batch_size
+            param.grad.zero_()
+
+lr = 0.03
+num_epochs = 3
+net = linreg
+loss = squared_loss
+
+for epoch in range(num_epochs):
+    for X, y in data_iter(batch_size, features, labels):
+        l = loss(net(X, w, b), y)   # 'X'和'Y'的小批量损失
+        # 因为‘l’形状是（'batch_size', l），而不是一个标量。‘l’中的所有元素被加到同一维
+        # 并以此计算关于[‘w’， ‘b’]的梯度
+        l.sum().backward()
+        sgd([w, b], lr, batch_size) #使用参数的梯度更新
+    with torch.no_grad():
+        train_l = loss(net(features, w, b), labels)
+        print(f'epoch {epoch + 1}, loss {float(train_l.mean()):f}')
+
+print(f'w的估计误差：{true_w - w.reshape(true_w.shape)}')
+print(f'b的估计误差：{true_b - b}')
